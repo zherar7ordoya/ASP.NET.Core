e7f8111e893f33ef5840a0c0a0910bf885eacfc4
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-01-21 20:00:17 -0300

    Chapter 02

--- 02 - Getting Started/xFirstProject/Controllers/HomeController.cs
+++ 02 - Getting Started/xFirstProject/Controllers/HomeController.cs
@@ -1,31 +1,15 @@
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using xFirstProject.Models;
 
-namespace xFirstProject.Controllers;
+
+namespace FirstProject.Controllers;
+
 
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public ViewResult Index()
     {
-        _logger = logger;
-    }
-
-    public IActionResult Index()
-    {
-        return View();
-    }
-
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        int hour = DateTime.Now.Hour;
+        string viewModel = hour < 12 ? "Good Morning" : "Good Afternoon";
+        return View("MyView", viewModel);
     }
 }
