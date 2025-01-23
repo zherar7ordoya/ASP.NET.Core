b2c02f31736451088131f0477809e0899fa5260d
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-01-23 12:18:59 -0300

    Party Invite

---
+++ 03 - Your First ASP.NET Core Application/xPartyInvites/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace xPartyInvites.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
