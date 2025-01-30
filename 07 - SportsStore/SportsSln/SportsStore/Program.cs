1b53bb4e9c2fb880dc147099a7dbf37bd5b3407c
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-01-30 20:09:00 -0300

    C07

--- 07 - SportsStore/SportsSln/SportsStore/Program.cs
+++ 07 - SportsStore/SportsSln/SportsStore/Program.cs
@@ -1,6 +1,18 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews(); // Add MVC services to the container
+
+builder.Services.AddDbContext<StoreDbContext>(opts => 
+{
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();                       // Enable static files
+app.MapDefaultControllerRoute();            // Default route for MVC
 
 app.Run();
