e0303f3595417524b6e0bb5dbf3159720b367bfd
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-02-04 12:56:26 -0300

    C08

---
+++ 08 - SportsStore - 2/SportsSln/SportsStore/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+
+namespace SportsStore.Controllers;
+
+public class HomeController(IStoreRepository repo) : Controller
+{
+    private readonly IStoreRepository repository = repo;
+    public int PageSize = 4;
+
+    public ViewResult Index(string? category, int productPage = 1)
+           => View(new ProductsListViewModel
+           {
+               Products = 
+                    repository.Products
+                        .Where(p => category == null || p.Category == category)
+                        .OrderBy(p => p.ProductID)
+                        .Skip((productPage - 1) * PageSize)
+                        .Take(PageSize),
+               PagingInfo = 
+                   new PagingInfo
+                   {
+                       CurrentPage = productPage,
+                       ItemsPerPage = PageSize,
+                       TotalItems = repository.Products.Count()
+                   },
+               CurrentCategory = category
+           });
+}
+
