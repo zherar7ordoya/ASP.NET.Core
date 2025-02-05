250227616ba7e7b08292d6b8c91c3b3005d1a609
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-02-05 12:58:42 -0300

    Tag Helpers

---
+++ 08 - SportsStore - 2/TagHelper/Controllers/ContactController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Text.Json;
+using ContactManager.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ContactManager.Controllers;
+
+public class ContactController : Controller
+{
+    public IActionResult Index()
+    {
+        string pathData = "Data/collection.json";
+        using var jsonFile = System.IO.File.OpenRead(pathData);
+
+        var contacts = JsonSerializer.Deserialize<List<Contact>>(jsonFile);
+
+        if (contacts == null)
+        {
+            return NotFound();
+        }
+
+        return View(contacts);
+    }
+
+    public IActionResult Detail(Guid? id)
+    {
+        string pathData = "Data/collection.json";
+        using var jsonFile = System.IO.File.OpenRead(pathData);
+
+        var contacts = JsonSerializer.Deserialize<List<Contact>>(jsonFile);
+
+        if (id == null || contacts == null)
+        {
+            return NotFound();
+        }
+
+        var contact = contacts.FirstOrDefault(x => x.Id == id);
+
+        if (contact == null)
+        {
+            return NotFound();
+        }
+        return View(contact);
+    }
+
+    //..........................................................................
+}
