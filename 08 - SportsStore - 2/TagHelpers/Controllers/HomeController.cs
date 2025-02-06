45f6d4f3a7e8ec4a4f4abc44e31125d316f9d735
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-02-06 12:59:37 -0300

    C08

---
+++ 08 - SportsStore - 2/TagHelpers/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace ContactManager.Controllers;
+
+
+public class HomeController : Controller
+{
+
+    public IActionResult Index()
+    {
+        // La idea es que, cuando se cargue la página de inicio, se redirija
+        // automáticamente a la lista de contactos.
+        return RedirectToAction("Index", "Contact");
+    }
+}
---
+++ 08 - SportsStore - 2/TagHelpers/TagHelpers/EmailTagHelper.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace ContactManager.TagHelpers;
+
+public class EmailTagHelper : TagHelper
+{
+    private const string EmailDomain = "mymail.com";
+    public required string MailTo { get; set; }
+
+    // Sobreescribiento el método Process de la clase TagHelper.
+    public override void Process(TagHelperContext context, TagHelperOutput output)
+    {
+        // Construyendo la dirección de correo electrónico.
+        var address = MailTo + "@" + EmailDomain;
+
+        // El tag es un enlace <a>...
+        output.TagName = "a";
+        // ...con el atributo href...
+        output.Attributes.SetAttribute("href", "mailto:" + address);
+        // ...que contiene la dirección de correo electrónico.
+        output.Content.SetContent(address);
+    }
+}
