9c88724d99a3f9b033c09f5309cf1cdb9b4afabf
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-02-10 19:49:17 -0300

    C08

--- 08 - SportsStore - 2/SportsSln/SportsStore/Pages/Cart.cshtml.cs
+++ 08 - SportsStore - 2/SportsSln/SportsStore/Pages/Cart.cshtml.cs
@@ -1,12 +1,40 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using SportsStore.Infrastructure;
+using SportsStore.Models;
 
-namespace SportsStore.Pages
+
+namespace SportsStore.Pages;
+
+
+public class CartModel(IStoreRepository repo) : PageModel
 {
-    public class CartModel : PageModel
+    private readonly IStoreRepository repository = repo;
+
+    public Cart? Cart { get; set; }
+    public string ReturnUrl { get; set; } = "/";
+
+    //..................................
+    // This is GET, stupid!
+    public void OnGet(string returnUrl)
     {
-        public void OnGet()
+        ReturnUrl = returnUrl ?? "/";
+        Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+    }
+
+    //..................................
+    // This is POST, stupid!
+    public IActionResult OnPost(long productId, string returnUrl)
+    {
+        Product? product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
+
+        if (product != null)
         {
+            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+            Cart.AddItem(product, 1);
+            HttpContext.Session.SetJson("cart", Cart);
         }
+
+        return RedirectToPage(new { returnUrl });
     }
 }
