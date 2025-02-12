1f8dda42558a86ad6bdcfffec94f6c2726e4f451
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-02-12 08:13:20 -0300

    C09

--- 09 - SportsStore - 3/SportsSln/SportsStore/Pages/Cart.cshtml.cs
+++ 09 - SportsStore - 3/SportsSln/SportsStore/Pages/Cart.cshtml.cs
@@ -3,38 +3,38 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using SportsStore.Infrastructure;
 using SportsStore.Models;
 
-
 namespace SportsStore.Pages;
 
-
-public class CartModel(IStoreRepository repo) : PageModel
+public class CartModel(IStoreRepository repo, Cart cartService) : PageModel
 {
     private readonly IStoreRepository repository = repo;
 
-    public Cart? Cart { get; set; }
+    public Cart Cart { get; set; } = cartService;
     public string ReturnUrl { get; set; } = "/";
 
-    //..................................
-    // This is GET, stupid!
+    // This is the GET
     public void OnGet(string returnUrl)
     {
         ReturnUrl = returnUrl ?? "/";
-        Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
+        //Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart(); 
     }
 
-    //..................................
-    // This is POST, stupid!
+    // This is the POST
     public IActionResult OnPost(long productId, string returnUrl)
     {
         Product? product = repository.Products.FirstOrDefault(p => p.ProductID == productId);
 
         if (product != null)
         {
-            Cart = HttpContext.Session.GetJson<Cart>("cart") ?? new Cart();
             Cart.AddItem(product, 1);
-            HttpContext.Session.SetJson("cart", Cart);
         }
 
+        return RedirectToPage(new { returnUrl });
+    }
+
+    public IActionResult OnPostRemove(long productId, string returnUrl)
+    {
+        Cart.RemoveLine(Cart.Lines.First(cl => cl.Product.ProductID == productId).Product);
         return RedirectToPage(new { returnUrl });
     }
 }
