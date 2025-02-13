f551210cee37ea14a858737a6f3b491263d87c47
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-02-13 12:53:04 -0300

    C09

--- 09 - SportsStore - 3/SportsSln/SportsStore/Models/StoreDbContext.cs
+++ 09 - SportsStore - 3/SportsSln/SportsStore/Models/StoreDbContext.cs
@@ -6,4 +6,5 @@ namespace SportsStore.Models;
 public class StoreDbContext(DbContextOptions<StoreDbContext> options) : DbContext(options)
 {
     public DbSet<Product> Products => Set<Product>();
+    public DbSet<Order> Orders => Set<Order>();
 }
