47251be20a0b5bcc8f3c2646e462ef40c149bd9d
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-02-14 08:22:08 -0300

    Probando commit desde Visual Studio.

--- 10 - SportsStore - 4/SportsSln/SportsStore/Program.cs
+++ 10 - SportsStore - 4/SportsSln/SportsStore/Program.cs
@@ -59,7 +59,8 @@ app.MapDefaultControllerRoute();
 // Rutas por defecto para Razor Pages.
 app.MapRazorPages();
 app.MapBlazorHub();
-app.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index");
+
+app.MapFallbackToPage("/admin/{*catchall}", "/Admin/Index"); // Revisar coloreado.
 
 // Sembrado de datos en la base si no existen.
 SeedData.EnsurePopulated(app);
