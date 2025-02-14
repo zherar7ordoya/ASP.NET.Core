41335e21dcbcf31108c1887b3719abbc2d54a6cc
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-02-14 00:51:18 -0300

    C10

---
+++ 10 - SportsStore - 4/SportsSln/SportsStore/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers;
+
+public class OrderController(IOrderRepository repoService, Cart cartService) : Controller
+{
+    private readonly IOrderRepository repository = repoService;
+    private readonly Cart cart = cartService;
+
+    public ViewResult Checkout() => View(new Order());
+
+    [HttpPost]
+    public IActionResult Checkout(Order order)
+    {
+        if (cart.Lines.Count == 0)
+        {
+            ModelState.AddModelError("", "Sorry, your cart is empty!");
+        }
+        if (ModelState.IsValid)
+        {
+            order.Lines = [.. cart.Lines];
+            repository.SaveOrder(order);
+            cart.Clear();
+            return RedirectToPage("/Completed", new { orderId = order.OrderID });
+        }
+        else
+        {
+            return View();
+        }
+    }
+}
