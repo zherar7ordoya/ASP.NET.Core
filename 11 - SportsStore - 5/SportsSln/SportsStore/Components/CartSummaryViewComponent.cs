1aba53c6292308acb8310512da5b4197b0be11b5
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-02-18 08:47:55 -0300

    C11

---
+++ 11 - SportsStore - 5/SportsSln/SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using SportsStore.Models;
+
+namespace SportsStore.Components;
+
+public class CartSummaryViewComponent(Cart cartService) : ViewComponent
+{
+    private readonly Cart cart = cartService;
+
+    public IViewComponentResult Invoke()
+    {
+        return View(cart);
+    }
+}
---
+++ 11 - SportsStore - 5/SportsSln/SportsStore/Models/Cart.cs
@@ -0,0 +1,35 @@
+﻿namespace SportsStore.Models;
+
+public class Cart
+{
+    public List<CartLine> Lines { get; set; } = [];
+
+    public virtual void AddItem(Product product, int quantity)
+    {
+        CartLine? line = Lines
+            .Where(p => p.Product.ProductID == product.ProductID)
+            .FirstOrDefault();
+
+        if (line == null)
+        {
+            Lines.Add(new CartLine
+            {
+                Product = product,
+                Quantity = quantity
+            });
+        }
+        else
+        {
+            line.Quantity += quantity;
+        }
+    }
+
+    public virtual void RemoveLine(Product product) =>
+        Lines.RemoveAll(l => l.Product.ProductID == product.ProductID);
+
+    public decimal ComputeTotalValue() =>
+        Lines.Sum(e => e.Product.Price * e.Quantity);
+
+    public virtual void Clear() =>
+        Lines.Clear();
+}
