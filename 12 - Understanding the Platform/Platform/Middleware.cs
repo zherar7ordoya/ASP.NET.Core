e5d155180ed8d1e6bb509b12511dcf073a13c0a3
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-02-24 19:42:16 -0300

    C12

---
+++ 12 - Understanding the Platform/Platform/Middleware.cs
@@ -0,0 +1,19 @@
+﻿namespace Platform;
+
+public class QueryStringMiddleWare(RequestDelegate nextDelegate)
+{
+    private readonly RequestDelegate next = nextDelegate;
+
+    public async Task Invoke(HttpContext context)
+    {
+        if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
+        {
+            if (!context.Response.HasStarted)
+            {
+                context.Response.ContentType = "text/plain";
+            }
+            await context.Response.WriteAsync("Class-based Middleware \n");
+        }
+        await next(context);
+    }
+}
--- 12 - Understanding the Platform/Platform/Program.cs
+++ 12 - Understanding the Platform/Platform/Program.cs
@@ -1,9 +1,27 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// http://localhost:5000/?custom=true
-app.Use(async (context, next) => {
+app.Use(async (context, next) =>
+{
+    await next();
+    await context.Response.WriteAsync($"\nStatus Code: {context.Response.StatusCode}");
+});
 
+app.Use(async (context, next) =>
+{
+    if (context.Request.Path == "/short")
+    {
+        await context.Response.WriteAsync($"Request Short Circuited");
+    }
+    else
+    {
+        await next();
+    }
+});
+
+// http://localhost:5000/?custom=true
+app.Use(async (context, next) =>
+{
     if (context.Request.Method == HttpMethods.Get && context.Request.Query["custom"] == "true")
     {
         context.Response.ContentType = "text/plain";
@@ -12,6 +30,8 @@ app.Use(async (context, next) => {
 
     await next();
 });
+
+app.UseMiddleware<Platform.QueryStringMiddleWare>();
 
 app.MapGet("/", () => "Hello World!");
 
