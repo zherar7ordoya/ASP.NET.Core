6fcce1ea137ab3143fdd1b74fd5245dd74df2f0e
Gerardo Tordoya <zherar7ordoya@outlook.com>
2025-03-10 19:52:28 -0300

    Kestrel

--- 14 - Dependency Injection/PlatformC14/Program.cs
+++ 14 - Dependency Injection/PlatformC14/Program.cs
@@ -1,17 +1,30 @@
 using Platform;
 using Platform.Services;
-var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
-var app = builder.Build();
-app.UseMiddleware<WeatherMiddleware>();
-app.MapGet("middleware/function", async (HttpContext context,
-        IResponseFormatter formatter) => {
+using System.Diagnostics;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        builder.Services.AddSingleton<IResponseFormatter, HtmlResponseFormatter>();
+
+        var app = builder.Build();
+        app.UseMiddleware<WeatherMiddleware>();
+
+        // Endpoint functions
+        app.MapGet("middleware/function", async (HttpContext context, IResponseFormatter formatter) =>
+        {
             await formatter.Format(context, "Middleware Function: It is snowing in Chicago");
         });
-//app.MapGet("endpoint/class", WeatherEndpoint.Endpoint); 
-//app.MapWeather("endpoint/class");
-app.MapGet("endpoint/function", async (HttpContext context,
-        IResponseFormatter formatter) => {
+
+        app.MapGet("endpoint/function", async (HttpContext context, IResponseFormatter formatter) =>
+        {
             await formatter.Format(context, "Endpoint Function: It is sunny in LA");
         });
-app.Run();
+
+        app.MapGet("/", () => $"Worker Process Name: {Process.GetCurrentProcess().ProcessName}");
+
+        app.Run();
+    }
+}
